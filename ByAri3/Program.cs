117c6f0a8c964971b9e5d1ca0a722acce2536d2e
Jaime Javier León Murrieta <jaimeleonm@outlook.com>
2022-04-05 12:07:18 -0500

    Ahora si ya jala chido el Distinct

--- ByAri3/Program.cs
+++ ByAri3/Program.cs
@@ -9,7 +9,7 @@ namespace ByAri3
         static void Main(string[] args)
         {
             //var arrayLength = int.Parse(Console.ReadLine());
-            int[] arr = new int[] { -1, 2, 5, 1, -5, 3, -5, 5, -3, 3 };
+            int[] arr = new int[] { -1, -3, 2, 5, 1, -5, 3, -5, 3, 5 };
 
             var r = Result(arr);
 
@@ -36,7 +36,24 @@ namespace ByAri3
                 }
             }
 
-            return result.Distinct().ToArray();
+            return Distinct(result.ToArray());
+        }
+
+        private static int[] Distinct(int[] arr)
+        {
+            List<int> result = new List<int>();
+
+            result.Add(arr[0]);
+
+            for (int j = 1; j < arr.Length-1; j++)
+            {
+                if (arr[j-1] != arr[j])
+                {
+                    result.Add(arr[j]);
+                }
+            }
+
+            return result.ToArray();
         }
     }
 }
